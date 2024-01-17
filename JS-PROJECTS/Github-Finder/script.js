9a26690ca35716a02cd8c7b0c877fcff2c1ee082
SangBong Lee <146823015+HiImLeon1@users.noreply.github.com>
2024-01-17 15:37:17 +0900

    Add files via upload

--- JS-PROJECTS/Github-Finder/script.js
+++ JS-PROJECTS/Github-Finder/script.js
@@ -1,7 +1,13 @@
-function displayUser(){
-    const usercomp = document.getElementById('nameinput').value;
-const namecon = document.getElementById('p1');
-const usernameEl = document.createElement('p');
-usernameEl.innerHTML = usercomp;
-namecon.appendChild(usernameEl);
+let bt1 = document.getElementById("#r1");
+let bt2 = document.getElementById("#r2");
+let bt3 = document.getElementById("#r3");
+
+function clicked1() {
+    window.location.href="https://github.com/HiImLeon1/JavaScript"
+}
+function clicked2() {
+    window.location.href="https://github.com/HiImLeon1/HTML-CSS"
+}
+function clicked3() {
+    window.location.href= "https://github.com/HiImLeon1/Java_Basic"
 }
